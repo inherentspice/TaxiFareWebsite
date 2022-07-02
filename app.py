f040cfb019c6c184757ff12c04cad129722ace7f
Stephen Smith <stephenlehanesmith@gmail.com>
2022-07-01 23:20:25 -0700

    added image

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import requests
 # TaxiFareModel front
 
 st.title("New York Taxi Fare Prediction")
-st.image("https://a.co/d/gc3ceCd")
+st.image("https://t1.pixers.pics/img-d5043af1/canvas-prints-cartoon-monster-truck.png?H4sIAAAAAAAAA5VPy26EMAz8HZAAm7zhA_a6n4BCMLu0AaKEttv9-gZVvVTqofLB9tie8cDbluxM4Gg7KMK6TJMnmBefu9RHSsuTCqykFmWfUV8gYtnv7xRd3ENRi66qJatU21YKTdl_2Hy42vha3I8jpB4g8SYsj8yWk0vg1gQMWw3IQZppIt0KxTtBQ_D7se01xwfHJmy3Cs8oexuC_xwiZc1Eg_Xhbv9BLtnMtRj2Mdpn_Vui_HEoECtxOpv3_P1RnDP4Q-S7hrwOlysYA1KC0tDJExouV2OkVLqTgzLcMjlpJD2OrWCOke5Qjmhn1hrjmpdw-wIZxfYTgAEAAA==")
 date = st.date_input('select date')
 time = st.time_input('select the time')
 pickup_longitude = st.text_input('pickup longitude', value=-73.950655)
